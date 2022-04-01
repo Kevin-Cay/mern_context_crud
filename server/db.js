f21a153d3324ea4f449d2a3adc9cd5b74af85c1d
Kevin-Cay <kevin.cay1234@gmail.com>
2022-03-31 23:27:54 -0600

    db config

--- server/db.js
+++ server/db.js
@@ -4,6 +4,7 @@ import { MONGODB_URI } from './config.js';
 export async function connectDB() {
     try {
         const db = await mongose.connect(MONGODB_URI);
+        console.log('conect to database')
     } catch (error) {
         console.log(error);
     }
